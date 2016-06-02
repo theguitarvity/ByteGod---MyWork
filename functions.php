05352b535eaa8f2b5a163f41fb8ddec028344394
Mateus Gabi <mateusgabimoreira@hotmail.com>
2016-06-02 12:50:56 -0400

    Algumas alterações

--- functions.php
+++ functions.php
@@ -4,10 +4,10 @@ function stringToMinutes($string){
     
     $str = split(":", $string);
     
-    return $str[0]*60 + $str[1] + $str[2]/60;
+    return ceil($str[0]*60 + $str[1] + $str[2]/60);
   
 }
 
-?>
+
 
 
--- submitTest.php
+++ submitTest.php
@@ -2,13 +2,10 @@
 
 require './functions.php';
 
-$minutes = stringToMinutes($_POST["cronometro"]);
-
-
 // TODO sql para inserir no bd
-echo ceil($minutes);
+echo stringToMinutes($_POST["cronometro"]);
 
-?>
+
 
 
 
